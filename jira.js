b70df3df3a0356b593fee03517b46a87298f897e
Ani Channarasappa <ani@local>
2019-05-07 20:18:59 -0400

    Added loader and refresh on filter change

--- jira.js
+++ jira.js
@@ -1,33 +1,83 @@
 
 (function() {
 
-	const CSV_DOWNLOAD_URL = location.protocol + '//' + location.hostname + '/sr/jira.issueviews:searchrequest-csv-all-fields/temp/SearchRequest.csv?jqlQuery=order+by+created+DESC&tempMax=1000';
+  const FRAMES = [
+    '⠋',
+    '⠙',
+    '⠹',
+    '⠸',
+    '⠼',
+    '⠴',
+    '⠦',
+    '⠧',
+    '⠇',
+    '⠏'
+  ];
+  
+  const startLoader = () => {
 
-	const report = R.pipe(
-		R.path(['data']),
-		R.reduce(
-			(acc, issue) => {
+    let frameIndex = 0;
 
-				if (issue['Custom field (Story Points)'])
-					return R.assoc('story_points', acc.story_points + parseInt(issue['Custom field (Story Points)'], 10), acc)
+    return setInterval(() => {
 
-				return acc;
+      frameIndex = (frameIndex >= FRAMES.length - 1)
+      ? 0
+      : frameIndex + 1;
 
-			},
-			{
-				story_points: 0,
-			}
-		)
-	);
+      document.querySelector('[title="Sort By Story Points"]').textContent = 'Story Points (' + FRAMES[frameIndex] + ')';
 
-	fetch(CSV_DOWNLOAD_URL)
-	.then((r) => r.text())
-	.then((r) => Papa.parse(r, { header: true }))
-	.then(report)
-	.then(({ story_points }) => {
+    }, 80)
 
-		document.querySelector('[title="Sort By Story Points"]').textContent = 'Story Points (' + story_points + ')';
+  }
 
-	})
+  const report = (response) => response.data.reduce(
+    (acc, issue) => {
+
+      if (issue['Custom field (Story Points)'])
+        return {
+          ...acc,
+          story_points: acc.story_points + parseInt(issue['Custom field (Story Points)'], 10) || 0,
+        }
+
+      return acc;
+
+    },
+    {
+      story_points: 0,
+    }
+  )
+
+  const getAndUpdateStoryPoints = () => {
+
+    const loader = startLoader();
+    const csvDownloadUrl = location.protocol + '//' + location.hostname + '/sr/jira.issueviews:searchrequest-csv-all-fields/temp/SearchRequest.csv?jqlQuery=' + window.location.href.match(/http(s?):\/\/(.*)\/issues\/\?jql=(.*)/)[3];
+
+
+    return fetch(csvDownloadUrl)
+    .then((r) => r.text())
+    .then((r) => Papa.parse(r, { header: true }))
+    .then(report)
+    .then(({ story_points }) => {
+  
+      clearInterval(loader);
+      document.querySelector('[title="Sort By Story Points"]').textContent = 'Story Points (' + story_points + ')';
+  
+    })
+
+  };
+  
+  let currentUrl = window.location.href;
+
+  setInterval(() => {
+
+    if (window.location.href === currentUrl)
+      return
+
+    currentUrl = window.location.href;
+    getAndUpdateStoryPoints();
+
+  }, 500)
+
+  getAndUpdateStoryPoints();
 
 })();
